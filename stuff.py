520f71d5a2b3a036988d2b2f317d9485b3ca6f0d
Ben-Littell <89919066+Ben-Littell@users.noreply.github.com>
2021-10-21 09:11:14 -0400

    kinda works

--- stuff.py
+++ stuff.py
@@ -2,7 +2,7 @@
 import csv
 
 
-# prompt = input('Enter a query: ')
+prompt = input('Enter a query: ')
 
 
 def open_file():
@@ -18,21 +18,36 @@ def open_file():
             list2.append(val)
         for value in list2:
             for item in company_db:
-                list1.append(item[list2[value]])
-                company_db_dict.update({line1.split(',')[list2[value]]: list1[0:len(list1)-1]})
+                list1.append(item[value])
+                company_db_dict.update({line1.split(',')[value]: list1[0:len(list1)-1]})
 
     return company_db_dict
 
 
 def check_tokens(tokens, csv_dict):
     work = True
+    key_list = ['first', 'last', 'sex', 'age', 'date', 'salary', 'position']
+    lo_op_list = ['<', '>', '==', '!=', '<=', '>=']
     tokens_s = tokens.split()
-    if len(tokens_s) != 3 or len(tokens_s) != 7:
+    if len(tokens_s) != 3 and len(tokens_s) != 7:
         work = False
-    # elif
+    if len(tokens_s) == 3:
+        if tokens_s[0] not in key_list:
+            work = False
+        elif tokens_s[1] not in lo_op_list:
+            work = False
+    if len(tokens_s) == 7:
+        if tokens_s[4] not in key_list:
+            work = False
+        elif tokens_s[5] not in lo_op_list:
+            work = False
+    if work:
+        print('Valid')
+    else:
+        print('Not Valid')
 
 
 file = open_file()
 print(file)
 
-# check_tokens(prompt, file)
+check_tokens(prompt, file)
